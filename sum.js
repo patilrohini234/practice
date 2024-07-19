3e26c543794d77f9722c7cf20df3acf28ccc965f
patilrohini234 <patilrohini234@gmail.com>
2024-07-19 15:46:29 +0530

    addition log

--- sum.js
+++ sum.js
@@ -1,7 +1,7 @@
 const a = 10;
 const b = 20;
 const c = 30;
-console.log(a + b + c);
+console.log("Addition of 3 numbers", a + b + c);
 console.log("Hello develop rohini");
 console.log(a * b * c);
 
